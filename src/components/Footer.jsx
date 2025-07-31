d9b3e051872c41833a3a1f4d40ed98bf99b12b02
Wanderson <wandersoncampossilva2018@gmail.com>
2025-07-31 23:12:21 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -30,7 +30,7 @@ const Footer = () => {
   ]
 
   return (
-    <footer className="bg-gray-900 text-white">
+    <footer className="bg-gray-900 text-white my-6">
       {/* Main Footer Content */}
       <div className="clyon-container py-12">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
