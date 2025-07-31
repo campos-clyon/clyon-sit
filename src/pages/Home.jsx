0f6257ffa7f5a7a575fcc8ad545c692dccbe7d88
Wanderson <wandersoncampossilva2018@gmail.com>
2025-08-01 00:06:39 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -139,7 +139,7 @@ const Home = () => {
   <Button
     size="lg"
     variant="outline"
-    className="border-[#ffffff] text-white font-bold hover:bg-white hover:text-[#07a0bc] transition-colors"
+    className="border-[#ffffff] text-black font-bold hover:bg-white hover:text-[#07a0bc] transition-colors"
     asChild
   >
     <Link to="/orcamentos">Pedir Orçamento</Link>
