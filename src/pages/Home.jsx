0a1eced4fb01f8522224d19e56f22ecc627f4230
Wanderson <wandersoncampossilva2018@gmail.com>
2025-07-31 19:01:48 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -150,16 +150,16 @@ const Home = () => {
             <div className="flex flex-col sm:flex-row gap-4 justify-center mt-8 text-sm">
   <a
     href="tel:+351931632622"
-    className="flex items-center justify-center gap-2 px-4 py-2 text-[#000000] font-medium border border-[#38b6ff] rounded-full hover:bg-[#07a0bc] hover:text-white transition"
+    className="flex items-center justify-center gap-2 px-4 py-2 text-[#000000] font-medium border border-[#38b6ff] rounded-full hover:bg-[#ffffff] hover:text-white transition"
   >
     <Phone className="h-4 w-4" />
-    <span>+351 931 632 622</span>
+    <span>+351 931632622</span>
   </a>
   <a
     href="https://wa.me/351931632622"
     target="_blank"
     rel="noopener noreferrer"
-    className="flex items-center justify-center gap-2 px-4 py-2 text-[#000000] font-medium border border-[#38b6ff] rounded-full hover:bg-[#07a0bc] hover:text-white transition"
+    className="flex items-center justify-center gap-2 px-4 py-2 text-[#000000] font-medium border border-[#38b6ff] rounded-full hover:bg-[#ffffff] hover:text-white transition"
   >
     <MessageCircle className="h-4 w-4" />
     <span>WhatsApp</span>
